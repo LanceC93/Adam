21477dd12addcb6ca26f1ae8713a57cd3389f5ce
LanceC93 <42276366+LanceC93@users.noreply.github.com>
2025-02-08 19:03:38 -0500

    game works

--- Person.py
+++ Person.py
@@ -3,8 +3,9 @@ import pygame
 class Person:
     def __init__(self, image, x, y):
         self.image = image
-        self.food = 100
+        self.food = 60
         self.water = 33
         self.shelter = 10
         self.x = x
         self.y = y
+        self.wood = 0
